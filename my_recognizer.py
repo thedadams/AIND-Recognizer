1b8e93f655652f8d2ba4481d80081bc7ceced5be
Donnie Adams <donald.g.adams@me.com>
2017-04-05 15:38:43 -0700

    Add comments

--- my_recognizer.py
+++ my_recognizer.py
@@ -10,7 +10,7 @@ def recognize(models: dict, test_set: SinglesData):
    :param test_set: SinglesData object
    :return: (list, list)  as probabilities, guesses
        both lists are ordered by the test set word_id
-       probabilities is a list of dictionaries where each key a word and value is Log Liklihood
+       probabilities is a list of dictionaries where each key a word and value is Log Likelihood
            [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
             {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
             ]
@@ -20,6 +20,22 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    for item in sorted(test_set.get_all_sequences().keys()):
+        X, lengths = test_set.get_item_Xlengths(item)
+        probs = dict()
+        best_word = ""
+        best_log_prob = float("-inf")
+        for word, model in models.items():
+            try:
+                log_prob = model.score(X, lengths)
+                probs[word] = log_prob
+                # Keep track of the best word so far so we can add it to guesses
+                if best_log_prob < log_prob:
+                    best_log_prob = log_prob
+                    best_word = word
+            except Exception as e:
+                probs[word] = float("-inf")
+        # Add this data to the returned variables
+        probabilities.append(probs.copy())
+        guesses.append(best_word)
+    return probabilities, guesses
